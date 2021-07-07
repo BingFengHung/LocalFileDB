2e075eadcf20e7f0d2999d77be351788502c11a1
BingFengHung <joe94008@gmail.com>
2021-07-07 10:12:48 +0800

    <feat>: Implement CreateSchema method

---
+++ LocalFileDatabase/LocalFileDatabase/FileDatabase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace LocalFileDatabase
+{
+    public class FileDatabase : IFileDatabase
+    {
+        #region Properties
+
+        public string SchemaName { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructors
+        /// </summary>
+        /// <param name="schemaName"></param>
+        public FileDatabase(string schemaName)
+        {
+            SchemaName = schemaName;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool CreateSchema(string schemaName)
+        {
+            bool isExist = IsSchemaExist(schemaName);
+
+            // If not exist create directory in root project
+            if (!isExist)
+                Directory.CreateDirectory(schemaName);
+
+            return true;
+        }
+
+        public bool CreateTable(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteData(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InserData(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSchemaExist(string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SelectData(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateData(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
