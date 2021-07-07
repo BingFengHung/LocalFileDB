e8c6322c49122ad63c72452dc859f83e80aacf6a
BingFengHung <joe94008@gmail.com>
2021-07-07 10:01:48 +0800

    <feat>: Define IFileDatabase interface - CRUD method - SchemaName

---
+++ LocalFileDatabase/LocalFileDatabase/IFileDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalFileDatabase
+{
+    public interface IFileDatabase
+    {
+        string SchemaName { get; set; }
+
+        bool IsSchemaExist(string schemaName);
+
+        bool CreateTable(string tableName);
+
+        bool InserData(string tableName);
+
+        bool UpdateData(string tableName);
+
+        bool DeleteData(string tableName);
+
+        bool SelectData(string tableName);
+
+        bool CreateSchema(string tableName);
+    }
+}
